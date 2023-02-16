2968ec8a39fdb6f0a0422a2df5cab587eb5d5668
Volodymyr-bart <61430064+Volodymyr-bart@users.noreply.github.com>
2023-02-16 11:55:53 +0200

    install react, redux and edit project

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import { composeWithDevTools } from "@redux-devtools/extension";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+
+import thunk from "redux-thunk";
+import fileReducer from "./fileReducer";
+import userReducer from "./userReducer";
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  files: fileReducer,
+});
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
